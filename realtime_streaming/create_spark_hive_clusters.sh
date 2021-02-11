d40156e1820789faed89d27b20455e2c813dbdb9
Gaurav <gauravmpai@gmail.com>
2021-02-11 04:22:29 -0500

    Added python script to consume from Kafka Topics

--- realtime_streaming/create_spark_hive_clusters.sh
+++ realtime_streaming/create_spark_hive_clusters.sh
@@ -2,14 +2,14 @@ gcloud dataproc clusters create spark-etl \
 --region us-central1 \
 --zone us-central1-a \
 --scopes default \
---initialization-actions gs://gmp-etl/init-actions/kafka.sh \
+--initialization-actions gs://goog-dataproc-initialization-actions-us-central1/kafka/kafka.sh \
 --master-machine-type n1-standard-2 \
 --master-boot-disk-size 100 \
 --num-workers 2 \
 --worker-machine-type n1-standard-2 \
 --worker-boot-disk-size 100 \
---image-version 1.4-debian10 \
---optional-components ZOOKEEPER,ANACONDA,JUPYTER &&
+--image-version 2.0-debian10  \
+--optional-components ZOOKEEPER,JUPYTER &&
 
 gcloud dataproc clusters create hive-cluster \
 --region us-central1 \
--- realtime_streaming/test_streaming.py
+++ realtime_streaming/test_streaming.py
@@ -1,1 +1,31 @@
-import pyspark
+from pyspark import SparkContext,SparkConf
+from pyspark.sql import SQLContext, SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
+import datetime
+import time
+
+conf = SparkConf().setAppName("User_Cart_Logs").setMaster("yarn")
+sc = SparkContext(conf=conf)
+sqlcon = SQLContext(sc)
+spark = SparkSession.builder.appName("user-cart-streaming-analysis").getOrCreate()
+
+
+df = spark.readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "spark-etl-w-1:9092") \
+    .option("subscribe","user_browsing_logs") \
+    .load() \
+    .selectExpr("CAST(value as STRING)")
+
+query = df.writeStream \
+    .outputMode("append") \
+    .format("console") \
+    .start()
+
+query.awaitTermination()
+
+
+
+
